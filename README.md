7c958309bc6a8a2dea0b61a077868078dc0458e0
Li Lianjie Anthony <llja0112@gmail.com>
2020-12-27 23:07:06 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Singapore SARS-CoV2 pandemic estimates
+
+## Estimation of reproduction number usinge EpiNow2
+
+Scripts for estimation of reproduction number is in scripts folder.
+
+Singapore's COVID infections data is in the data folder. Data is stored as a CSV file.
+
+`cd scripts`
+Run `R` in folder.
+`source('covid.R', echo=TRUE)`
+`plot(estimates)`
+`source('covid_community.R', echo=TRUE)`
+`plot(estimates)`
+`source('covid_dorm.R', echo=TRUE)`
+`plot(estimates)`
+ 
---
+++ scripts/covid.R
@@ -0,0 +1,31 @@
+library(EpiNow2)
+library(dplyr)
+
+Covid2020 <- read.csv('../data/COVID_SG.csv')
+
+# jpeg('graph.jpg')
+# plot(as.incidence(Flu2009$incidence$I, dates=Flu2009$incidence$dates))
+# dev.off()
+
+reporting_delay <- estimate_delay(rlnorm(1000,  log(3), 1),
+                                  max_value = 15, bootstraps = 1)
+
+generation_time <- get_generation_time(disease = "SARS-CoV-2", source = "ganyani")
+incubation_period <- get_incubation_period(disease = "SARS-CoV-2", source = "lauer")
+
+
+reported_cases <- Covid2020 %>% select(Date_reported, New_cases) %>% rename(date=Date_reported, confirm=New_cases)
+
+reported_cases$date <- as.Date(reported_cases$date)
+
+estimates <- epinow(reported_cases = reported_cases, 
+                    generation_time = generation_time,
+                    delays = delay_opts(incubation_period, reporting_delay),
+                    rt = rt_opts(prior = list(mean = 2, sd = 0.2)),
+                    stan = stan_opts(cores = 4))
+
+names(estimates)
+
+knitr::kable(summary(estimates))
+
+head(summary(estimates, type = "parameters", params = "R"))
